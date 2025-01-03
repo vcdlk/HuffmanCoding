3ce32f554148577290de41f0e9c2cf84269b6d03
vcdlk <476041181@qq.com>
2025-01-03 14:32:04 +0800

    feat(huffman decode) add decode logic

---
+++ src/huffmandecode.cpp
@@ -0,0 +1,53 @@
+
+
+#include "bitiostream.h"
+#include "canonicalcode.h"
+#include "code_tree.h"
+#include "huffman_decoder.h"
+
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main(int argc, char** argv){
+  if(argc != 3){
+    std::cout << "Usage" << argv[0] << "InputFile OutputFile" << std::endl;
+  }
+
+  std::string input_file_name = argv[1];
+  std::string output_file_name = argv[2];
+
+  std::ifstream in(input_file_name, std::ios::binary);
+  std::ofstream out(output_file_name, std::ios::binary);
+  BitInputStream bin(in);
+
+  std::vector<uint32_t> code_lens;
+  for(int i = 0; i < 257; i++){
+    int b = in.get();
+    if(b == std::char_traits<char>::eof()){
+      break;
+    }
+    assert(0 <= b && b <= 256);
+    code_lens.push_back(b);
+  }
+  CanonicalCode canonicalcode(code_lens);
+  CodeTree code_tree = canonicalcode.ToCodeTree();
+
+  HuffmanDecoder huffman_decoder(bin);
+  huffman_decoder.SetCodeTree(&code_tree);
+
+  while(true){
+    int b = huffman_decoder.Read();
+    if(b == -1){
+      out.close();
+      break;
+    }
+    out.put(b);
+  }
+  return 0;
+}
